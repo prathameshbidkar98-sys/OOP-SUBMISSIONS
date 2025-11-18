07426ee2c407e10eedcd42f6caa85faced2a68d6
prathameshbidkar98-sys <prathameshbidkar98@gmail.com>
2025-11-18 18:42:21 +0530

    Create code1.cpp

---
+++ Experiment-12/code1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() { 
+    stack<int> s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    
+    cout << "Top element : " << s.top() << endl;
+    s.pop();
+    cout << "Top element after pop : " << s.top() << endl;
+    cout << "Stack size:" << s.size() << endl;
+    
+    if (s.empty()) {
+        cout << "Stack is empty" << endl;
+    } else {
+        cout << "Stack is not empty" << endl;
+    }
+    return 0;
+}
